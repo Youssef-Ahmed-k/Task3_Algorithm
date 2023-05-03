49f8ec0ec424859e166fc7706708a87325d4c9ce
youssefkota <104798903+Youssef-Ahmed-k@users.noreply.github.com>
2023-05-03 03:55:59 +0200

    update

---
+++ SOURCE CODE/non-recursive/main.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+int is_balanced(char* substr)
+{
+    int count[2] = {0};
+    for(int i = 0; i < strlen(substr); i++)
+    {
+        if(substr[i] == substr[0])
+            count[0]++;
+        else
+            count[1]++;
+    }
+    return (count[0] == count[1]);
+}
+
+int longest_balanced_substring(char* s)
+{
+    int n = strlen(s);
+    int max_length = 0;
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j = i+2; j <= n; j++)
+        {
+            char substr[j-i];
+            strncpy(substr, &s[i], j-i);
+            substr[j-i] = '\0';
+            if(is_balanced(substr) && strlen(substr) > max_length)
+                max_length = strlen(substr);
+        }
+    }
+    return max_length;
+}
+
+int main()
+{
+    printf("this code run by non-recursive\n");
+    printf("---------------------------------------\n");
+    char s1[] = "cabbacc";
+    printf("the longest balanced substring for ( %s ) = %d\n",s1,longest_balanced_substring(s1));  // Expected Output: 4
+
+    char s2[] = "abababa";
+    printf("the longest balanced substring for ( %s ) = %d\n",s2,longest_balanced_substring(s2));  // Expected Output: 6
+
+    char s3[] = "aaaaaaa";
+    printf("the longest balanced substring for ( %s ) = %d\n",s3,longest_balanced_substring(s3));  // Expected Output: 0
+
+    return 0;
+}
---
+++ SOURCE CODE/recursive/main.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+int is_balanced(int* freq) {
+    int i;
+    int count = 0;
+    int diff_chars = 0;
+    for (i = 0; i < 26; i++) {
+        if (freq[i] > 0) diff_chars++;
+        if (freq[i] == freq[0] && freq[i] > 0) count++;
+    }
+    if (diff_chars == 2 && count == 2) return 1;
+    return 0;
+}
+int longest_balanced_substring_helper(char* s, int start, int end) {
+    if (end - start + 1 < 2) {
+        return 0; // substring too short to be balanced
+    }
+    int freq[26] = {0};
+    int i;
+    for (i = start; i <= end; i++) {
+        freq[s[i] - 'a']++;
+    }
+    if (is_balanced(freq)) {
+        return end - start + 1; // this substring is balanced
+    }
+    int len1 = longest_balanced_substring_helper(s, start, end - 1);
+    int len2 = longest_balanced_substring_helper(s, start + 1, end);
+    return (len1 > len2) ? len1 : len2; // return the longer of the two balanced substrings
+}
+
+int longest_balanced_substring(char* s) {
+    return longest_balanced_substring_helper(s, 0, strlen(s) - 1);
+}
+int main() {
+     printf("this code run by Recursive\n");
+    printf("---------------------------------------\n");
+    char s1[] = "cabbacc";
+    printf("the longest balanced substring for ( %s ) = %d\n",s1 ,longest_balanced_substring(s1)); // Expected Output: 4
+
+    char s2[] = "abababa";
+    printf("the longest balanced substring for ( %s ) = %d\n",s2 ,longest_balanced_substring(s2)); // Expected Output: 6
+
+    char s3[] = "aaaaaaa";
+    printf("the longest balanced substring for ( %s ) = %d\n",s3 ,longest_balanced_substring(s3)); // Expected Output: 0
+
+    return 0;
+}
+
